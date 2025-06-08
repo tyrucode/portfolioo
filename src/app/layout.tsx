89c74bf4ef812088d719bd9c102df5ac8932b5e1
ruizt <ruiztyler24@gmail.com>
2025-06-08 11:56:49 -0400

    setup portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const roboto = Roboto({
+  weight: '700',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "Tyler Ruiz | Portfolio",
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
   return (
     <div>
-      <h1>hi</h1>
+      <TracingBeam>
+        <header>
+          <h1>Tyler Ruiz</h1>
+          <h2>Full Stack Portfolio</h2>
+          <h3>
+            I specialize in building dynamic and responsive web applications using technologies like Next.JS, React, PostgreSQL, and more!
+            Explore the rest of my portfolio to learn about me, my projects, and my experience.
+          </h3>
+        </header>
+        <div>
+          {/* More content can go here */}
+        </div>
+      </TracingBeam>
     </div>
   );
 }
